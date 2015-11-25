b1445a0bab7584d00eb823dd069331716954d6f1
abhinavtk97 <abhinavtk97@gmail.com>
2015-11-26 01:09:28 +0530

    Final Version For Submisiion
    Signed-off-by: abhinavtk97 <abhinavtk97@gmail.com>

---
+++ engine_select.py
@@ -0,0 +1,28 @@
+def server_select(v):
+    if v==1:
+        
+        a="Google"
+        b="https://www.google.com/?gfe_rd=cr&ei=qOnZVf3qM8mL8AX_9rvwBg&gws_rd=cr&fg=1#q="
+        
+    if v==2:
+        a="Bing"
+        b="https://www.bing.com/search?q="
+        
+    if v==3:
+        a="Yahoo"
+        b="https://search.yahoo.com/search?n=10&ei=UTF-8&va_vt=any&vo_vt=any&ve_vt=any&vp_vt=any&vst=0&vf=all&vm=i&fl=0&p="
+        
+    return a
+def url_select(v):
+    if v==1:
+        b="https://www.google.com/?gfe_rd=cr&ei=qOnZVf3qM8mL8AX_9rvwBg&gws_rd=cr&fg=1#q="
+        
+    if v==2:
+        b="https://www.bing.com/search?q="
+        
+    if v==3:
+        b="https://search.yahoo.com/search?n=10&ei=UTF-8&va_vt=any&vo_vt=any&ve_vt=any&vp_vt=any&vst=0&vf=all&vm=i&fl=0&p="
+        
+    return b
+
+    
--- web.py
+++ web.py
@@ -1,42 +1,89 @@
-import Tkinter
+
+#Importing Essential Modules
 from Tkinter import *
 import webbrowser
-	 
-engine_dict = {'google': "https://www.google.com/?gfe_rd=cr&ei=qOnZVf3qM8mL8AX_9rvwBg&gws_rd=cr&fg=1#q=",
-	               'bing': "https://www.bing.com/search?q=",
-	               'yahoo': "https://search.yahoo.com/search?n=10&ei=UTF-8&va_vt=any&vo_vt=any&ve_vt=any&vp_vt=any&vst=0&vf=all&vm=i&fl=0&p="}
-	 
-def pick_engine():
-    question = "Pick your favorite search engine between Google, Bing, and Yahoo."
-    print (question)
-    while True:
-	        search_engine = raw_input('> ').lower()
-	        if search_engine in engine_dict:
-	            engine_name = search_engine.capitalize()
-	            base_url = engine_dict[search_engine]
-	        else:
-	            print('Wrong answer! Try again.\n')
-	            print(question)
-	            continue
-	        return engine_name, base_url
-	 
-engine_name, base_url = pick_engine()
-	 
-root = Tk()
+import engine_select
 
-root.title('Search in ' + engine_name)
-	 
-String_Entry = Entry(root)
-String_Entry.grid(row=0, column=0)
-	 
+#Select search engine
+def engine():
+    v=(var.get())
+    engine_name=engine_select.server_select(v)
+
+#Selecting the image of engine
+    if engine_name=="Google":
+     image1=PhotoImage(file="res/google.gif")
+     cwgt.img=image1
+     cwgt.create_image(115,110,anchor=NW,image=image1)
+    if engine_name=="Bing":
+     image1=PhotoImage(file="res/bing.gif")
+     cwgt.img=image1
+     cwgt.create_image(40,110,anchor=NW,image=image1)
+    if engine_name=="Yahoo":
+     image1=PhotoImage(file="res/yahoo.gif")
+     cwgt.img=image1
+     cwgt.create_image(25,110,anchor=NW,image=image1)
+    sel(engine_name)
+    
+def sel(engine_name):
+    selection = text="Enter your query here to search in "+engine_name
+    label.config(text = selection)
+
+#Search function
 def search():
-	    url = base_url + str(String_Entry.get())
-	    webbrowser.open(url)
-	   
-Search_Button = Button(root, text='Search', command=search)
-Search_Button.grid(row=0, column=1)
-	 
-Quit_Button = Button(root, text='Quit', command=quit)
-Quit_Button.grid(row=0, column=2)
-	 
-mainloop()
+    v=(var.get())
+    base_url=engine_select.url_select(v)
+    url = base_url + str(c2.get())
+    webbrowser.open(url)
+    
+#Bringing up new Window
+root = Tk()   
+root.title("Web Search")
+label = Label(root)
+label.pack()
+label.config(text="Select any one of the search engines")
+cwgt=Canvas(root)
+cwgt.configure(background='white')
+cwgt.pack(expand=True, fill=BOTH)
+
+#Making Radio Buttons
+var = IntVar()
+R1 = Radiobutton(root, text="Google", variable=var, indicatoron = 0,
+                width = 10,
+                padx = 20,value=1,
+                  command=engine)
+cwgt.create_window(10,20,window=R1, anchor=NW)
+
+R2 = Radiobutton(root, text="Bing", variable=var, indicatoron = 0,
+                width = 10,
+                padx = 20,value=2,
+                  command=engine)
+cwgt.create_window(130,20,window=R2, anchor=NW)
+
+R3 = Radiobutton(root, text="Yahoo",variable=var, indicatoron = 0,
+                width = 10,
+                padx = 20,value=3,
+                  command=engine)
+cwgt.create_window(250,20,window=R3, anchor=NW)
+
+engine_name="Google"
+
+#Inserting query box
+c2 = Entry(root)
+cwgt.create_window(127,60,window=c2, anchor=NW)
+v=1
+
+
+#Insrting Search Button	    
+a1 = Button(root, text='Search', command=search)
+cwgt.create_window(140,85,window=a1, anchor=NW)
+
+#Inserting Quit Button
+a2 = Button(root, text='Quit', command=quit)
+cwgt.create_window(210,85,window=a2, anchor=NW)	 
+
+#Credits
+cred=Button(cwgt,text="Developed by Abhinav T K, Earnest, Amrutha ",bd=0)
+cwgt.create_window(75,240,window=cred,anchor=NW)
+cred.configure(background='white')
+
+root.mainloop()
